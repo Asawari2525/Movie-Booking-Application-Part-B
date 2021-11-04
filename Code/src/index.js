080b8f3cd5bd54025a2362090cc05a72f5162f6a
Asawari2525 <baishflypi255@gmail.com>
2021-11-04 13:30:22 +0530

    checkpoint 1

--- Code/src/index.js
+++ Code/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
+//import registerServiceWorker from './registerServiceWorker';
+//import Controller from './screens/Controller';
 
 ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
